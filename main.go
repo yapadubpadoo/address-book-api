d12cf3af02ff43af705510bf176e655a01ebcf33
Hiruma <yapadubpadoo@gmail.com>
2018-07-22 14:12:50 +0700

    refactor

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+
+	mgo "gopkg.in/mgo.v2"
 )
 
 // Address ...
@@ -23,6 +26,8 @@ type AddressResponse struct {
 	Error Error
 }
 
+var dbSession = getDBSession()
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
 }
@@ -37,33 +42,53 @@ func handleResponse(w http.ResponseWriter, adr AddressResponse) {
 	fmt.Fprint(w, string(addressResponseJSON))
 }
 
+func validateJSONRequest(r *http.Request) (bool, string) {
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		return false, "Request body must be JSON"
+	}
+	return true, ""
+}
+
 func createAddress(w http.ResponseWriter, r *http.Request) {
 	address := Address{}
 	responseError := Error{}
 	addressResponse := AddressResponse{}
-	switch r.Method {
-	case "POST":
-		body, _ := ioutil.ReadAll(r.Body)
-		contentType := r.Header.Get("Content-Type")
-		if contentType != "application/json" {
-			fmt.Println("Invalid content type", contentType)
-			responseError.Code = -1
-			responseError.Message = "Request body must be JSON"
-			fmt.Println(responseError)
-		} else {
+	valid, err := validateJSONRequest(r)
+	if valid {
+		switch r.Method {
+		case "POST":
+			body, _ := ioutil.ReadAll(r.Body)
 			fmt.Println("Going to decode JSON")
 			error := json.Unmarshal(body, &address)
-			if error != nill {
+			if error != nil {
 				responseError.Message = fmt.Sprintf("%+v", error)
+			} else {
+				c := dbSession.DB("address_book").C("people")
+				err := c.Insert(address)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 			addressResponse.Data = append(addressResponse.Data, address)
+		default:
+			responseError.Code = 403
+			responseError.Message = fmt.Sprintf("%s is not allowed on this endpoint", r.Method)
 		}
-	default:
-		responseError.Code = 403
-		responseError.Message = fmt.Sprintf("%s is not allowed on this endpoint", r.Method)
+	} else {
+		responseError.Code = -1
+		responseError.Message = fmt.Sprintf("%s", err)
 	}
 	addressResponse.Error = responseError
 	handleResponse(w, addressResponse)
+}
+
+func getDBSession() *mgo.Session {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return session
 }
 
 func main() {
